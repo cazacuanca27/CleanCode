e38f441f0f3c729ce05a3eb071b93e2a2425516f
alinabrinza95 <st.ariina@yahoo.com>
2017-09-15 15:38:35 +0300

    last commit
    last commit

--- shanuMVCUserRoles/Controllers/UsersController.cs
+++ shanuMVCUserRoles/Controllers/UsersController.cs
@@ -104,11 +104,14 @@ namespace shanuMVCUserRoles.Controllers
 			{
 				var user = User.Identity;
 				ViewBag.Name = user.Name;
+                
+
 				//	ApplicationDbContext context = new ApplicationDbContext();
 				//	var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
 				//var s=	UserManager.GetRoles(user.GetUserId());
 				ViewBag.displayMenu = "No";
+                    
 
 				if (isAdminUser())
 				{
